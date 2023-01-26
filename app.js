e660a3393d9e32827d998d2a8b976e63ebf3480a
AhmedAli4821 <ahmadali4821@gmail.com>
2023-01-26 12:10:45 -0800

    my first fill java scrip

---
+++ app.js
@@ -0,0 +1,20 @@
+// ****Chapter 1
+// Question 1
+
+// alert("Err! Please enter a valid password")
+
+// Question 2
+
+// alert("Wellcome to JS Land... \n Happy Coding!")
+
+// Question 3
+
+// alert("Wellcome to JS Land...")
+
+// Question 4
+
+// alert("Happy Coading \n prevent this page frome creating additional dialogs")
+
+// Question 5
+
+alert("Hello...I can run JS through my web brouser's console")
